1c0a8c1f11a5b56f4e0ccd96a372ee84b7cf7223
leiys13 <leiys13@icloud.com>
2018-04-20 00:03:02 +0800

    正则表达式工具类

--- src/main/java/com/sadasen/util/CollectionUtil.java
+++ src/main/java/com/sadasen/util/CollectionUtil.java
@@ -1,19 +1,29 @@
 package com.sadasen.util;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 /**
  * @date 2018年3月23日
  * @author lei.ys
  * @addr company
- * @desc
+ * @desc 集合类工具类
  */
-public class CollectionUtil {
+public class CollectionUtil implements Serializable {
 	
+	private static final long serialVersionUID = -8798105551876918582L;
+
 	public static boolean isEmpty(Collection<?> coll) {
 		if(null==coll || coll.size()==0)
 			return true;
 		return false;
+	}
+	
+	public static boolean isNotEmpty(Collection<?> coll) {
+		return !isEmpty(coll);
+	}
+	
+	private CollectionUtil() {
 	}
 
 }
